6a454f80c9b391a4f634a18dbfd978e9a54a539e
Lucas Monteiro de Sousa <107234259+LMsousa20@users.noreply.github.com>
2022-07-18 22:10:23 -0300

    lucas adicionou botao magico

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,32 +1,35 @@
 import { useState } from "react";
-import {auth} from "../../Api";
-import {createUserWithEmailAndPassword} from 'firebase/auth';
+import { auth } from "../../Api";
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from "react-router-dom";
 import './index.css';
+import medidor from '../../assets/img/Medidor.png';
+import bcgEdge from '../../assets/img/backgroundEdge.png'
+
 const SignUp = () => {
     let navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [password2, setPassword2] = useState('');
-    
-    
-    const VerifyPassword=()=>{
-        if(password ===password2 && email.length>0){
+
+
+    const VerifyPassword = () => {
+        if (password === password2 && email.length > 0) {
             handleSignUp();
-        }else{
+        } else {
             alert("Atenção:Email incompleto ou senha distintas . Por favor, confira a senha digitada nos dois campos")
         }
     }
-    
-    
-    
+
+
+
     const handleSignUp = () => {
-        
+
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in
                 //const user = userCredential.user;
-                
+
                 console.log(userCredential);
                 navigate("/");
                 alert('Usuário cadastrado com sucesso');
@@ -36,17 +39,31 @@ const SignUp = () => {
                 const errorMessage = error.message;
                 // ..
                 console.log(errorMessage);
-                alert("Ocorreu um erro ao cadastrar o usuário."+errorMessage)
+                alert("Ocorreu um erro ao cadastrar o usuário." + errorMessage)
             });
 
     }
 
     return (
-        <>  
-            <input type="email" placeholder="Digite seu email" onChange={(e) => { setEmail(e.target.value) }}></input>
-            <input type="password" placeholder="Digite sua senha" onChange={(e) => { setPassword(e.target.value) }}></input>
-            <input type="password" placeholder="Digite sua senha" onChange={(e) => { setPassword2(e.target.value) }}></input>
-            <button className=".btn-login-cadastro-page" disabled={password.length<6 ||email.length===0} onClick={VerifyPassword}> Cadastrar</button>
+        <>
+            <div className="quadro">
+
+                <div className="login">
+                    <span className="Title">Crie um nova conta</span>
+                    <div className="sublogin">
+                        <input className="inpt" type="email" placeholder="Digite seu email" onChange={(e) => { setEmail(e.target.value) }}></input>
+                        <input className="inpt" type="password" placeholder="Digite sua senha - min 6 digitos" onChange={(e) => { setPassword(e.target.value) }}></input>
+                        <input className="inpt" type="password" placeholder="Digite sua senha - min 6 digitos" onChange={(e) => { setPassword2(e.target.value) }}></input>
+                        <button className="btn-login-cadastro-page" disabled={password.length < 6 || email.length === 0} onClick={VerifyPassword}> Cadastrar</button>
+                    </div>
+                </div>
+                <div className="logo">
+                    <img className="imgn" src={require('./Medidor.png')} />
+
+                </div>
+            </div>
+            <img className="bcgEdge" src={bcgEdge} />
+
         </>
     );
 }
